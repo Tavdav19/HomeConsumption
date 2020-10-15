fd94375a48a4ae8aca535007b54a9d8f88ec6d23
Tavdav19 <tavaresdavid19@gmail.com>
2020-10-15 02:54:41 +0100

    Electricity Consumption Activity Created edited and finished Instant Consumption and daily graph working

--- app/src/main/java/com/example/homeconsumption/MainEletricidade.kt
+++ app/src/main/java/com/example/homeconsumption/MainEletricidade.kt
@@ -1,20 +1,118 @@
 package com.example.homeconsumption
 
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.Color.*
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
+import android.view.View.VISIBLE
 import androidx.appcompat.app.AppCompatActivity
+import com.github.mikephil.charting.components.Description
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.XAxis.XAxisPosition
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.activity_main_eletricidade.*
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
+
+
 
 class MainEletricidade : AppCompatActivity() {
+    // Database reference pointing to root of database
+    val rootRef: DatabaseReference = FirebaseDatabase.getInstance().reference
+
+    // Database reference pointing to demo node
+    val powerRef: DatabaseReference = rootRef.child("Sensors/PowerInst")
+    val entries = ArrayList<BarEntry>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_eletricidade)
 
-
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener {
+            startActivity(Intent(this, MenuActivity::class.java))
+            overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right)
         }
-    }
-}
+        // Read from the database
+        powerRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val value = dataSnapshot.getValue(Int::class.java)
+                EletricidadeAtual.text = value.toString()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
+
+        val sdf = SimpleDateFormat("yyyy-MM-dd")
+        val dia: String = sdf.format(Date())
+
+        var maximo=0f
+        for (y in 0..23) {
+            val luzRef: DatabaseReference = rootRef.child("Sensors/Power/"+dia+"/"+y)
+        luzRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                var mediaHora = dataSnapshot.getValue(Float::class.java)
+
+                if (mediaHora!=null){
+                    entries.add(BarEntry(y.toFloat(), mediaHora.toString().toFloat()))
+                    maximo = maxOf(maximo, mediaHora)
+                }else(entries.add(BarEntry(y.toFloat(), 0f)))
+
+                val barDataSet = BarDataSet(entries, "")
+
+                    barDataSet.color=(WHITE)
+
+
+                val data = BarData(barDataSet)
+                barChart.data = data
+                barChart.setTouchEnabled(false)
+                barChart.getLegend().setEnabled(false)
+
+
+                val description: Description = barChart.getDescription()
+                description.setEnabled(false)
+
+                val xAxis: XAxis = barChart.getXAxis()
+                xAxis.position = XAxisPosition.BOTTOM
+                xAxis.textSize = 10f
+                xAxis.textColor = Color.WHITE
+                xAxis.setDrawAxisLine(true)
+                xAxis.setDrawGridLines(false)
+                xAxis.setLabelCount(24, false)
+
+                val leftAxis: YAxis = barChart.getAxisLeft()
+                barChart.getAxisRight().setEnabled(false);
+
+                leftAxis.setTextSize(10f); // set the text size
+                leftAxis.setAxisMinimum(0f); // start at zero
+
+                leftAxis.setAxisMaximum(maximo); // the axis maximum is 100
+                leftAxis.setTextColor(Color.WHITE);
+                leftAxis.setGranularity(1f); // interval 1
+                leftAxis.setLabelCount(11, true); // force 6 labels
+
+
+                barDataSet.setDrawValues(false)
+                kWh.setVisibility(VISIBLE)
+                horatext.setVisibility(VISIBLE)
+                barChart.animateY(10)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+            }
+        })
+
+            btnHistoricoEle.setOnClickListener {
+                startActivity(Intent(this, MensalEletricidade::class.java))
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+            }
+    }}}
