5ad3d17deb458fb5ebf944971ba089587efd75e7
Tavdav19 <tavaresdavid19@gmail.com>
2020-10-15 02:59:21 +0100

    Menu Activity Created and finished

--- app/src/main/java/com/example/homeconsumption/MenuActivity.kt
+++ app/src/main/java/com/example/homeconsumption/MenuActivity.kt
@@ -1,20 +1,42 @@
 package com.example.homeconsumption
 
+
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_menu.*
+
 
 class MenuActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth;
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
-        setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        auth = FirebaseAuth.getInstance();
+        Logout.setOnClickListener {
+            auth.signOut();
+            val intent = Intent(this, LoginActivity::class.java);
+            startActivity(intent);
+        }
+        btnEletricidade.setOnClickListener {
+            startActivity(Intent(this, MainEletricidade::class.java))
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+        }
+        btnAgua.setOnClickListener {
+            startActivity(Intent(this, MainAgua::class.java))
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+        }
+        btnResiduos.setOnClickListener {
+            startActivity(Intent(this, MainResiduos::class.java))
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+        }
+        btnDicas.setOnClickListener {
+            startActivity(Intent(this, MainDicas::class.java))
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+        }
         }
     }
-}
